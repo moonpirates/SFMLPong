2687e6cac1405c257f3f95a31dc024e8475ed7a6
Jordi <jordi@jordi.com>
2020-12-09 22:17:45 +0100

    Colored logs

--- SFMLPong/src/Macros/Macros.h
+++ SFMLPong/src/Macros/Macros.h
@@ -1,12 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-// Colors based on https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-#define __ANSI_COLOR_DEFAULT__ "henk";
-#define __ANSI_COLOR_GREY__ "henk";
+// Colors references: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+#define __ANSI_COLOR_DEFAULT__ "\033[0m"
+#define __ANSI_COLOR_TIME__ "\033[90m"
+#define __ANSI_COLOR_LOG__ "\033[91m"
+#define __ANSI_COLOR_FILE__ "\033[90m"
 
-#define LOG(x) std::cout << __ANSI_COLOR_GREY__ << "[" << __TIME__ << "] " << __ANSI_COLOR_DEFAULT__ << x << \
-	__ANSI_COLOR_GREY__ << __FILENAME__ << "(" << __LINE__ << ")" << __ANSI_COLOR_DEFAULT__ << std::endl;
+#define LOG(x) std::cout << std::left \
+	<< __ANSI_COLOR_TIME__ << "[" << __TIME__ << "] "  \
+	<< __ANSI_COLOR_FILE__ << __FILENAME__ << "(" << __LINE__ << ")" << " " \
+	<< __ANSI_COLOR_LOG__ << x << " " \
+	<< __ANSI_COLOR_DEFAULT__ << std::endl;
